60354dbb7302d5884b3430707713480950378a4d
shohibun <shohibunnajam@gmail.com>
2022-06-09 13:37:07 +0700

    update service API

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://binar-car.netlify.app/api/auth/";
 
 const register = (username, email, password) => {
   //Bakal return suatu object
